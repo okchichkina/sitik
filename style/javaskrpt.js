95046083c31dd306390cdc14594249c608702e7a
okchichkina <104198020+okchichkina@users.noreply.github.com>
2024-02-27 16:36:03 +0300

    Add files via upload

---
+++ style/javaskrpt.js
@@ -0,0 +1,9 @@
+$(function () {  
+    $('.menu a').each(function () { 
+        var location = window.location.href; 
+        var link = this.href;  
+        if(location == link) { 
+            $(this).addClass('active ');
+        }
+    });
+});
